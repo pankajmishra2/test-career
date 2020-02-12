8fa614426fad9011955cff70998b05fc2c069062
pankajmishra2 <pankaj.mishra@pearson.com>
2020-02-12 06:10:37 +0530

    first commit

---
+++ src/main/java/com/pankaj/TestCareerApplication.java
@@ -0,0 +1,13 @@
+package com.pankaj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCareerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestCareerApplication.class, args);
+	}
+
+}
